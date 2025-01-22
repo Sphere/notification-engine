415ac360d61bcd397d92ec56a488b9a302289589
Sumit Bajaj <sumit.bajaj@tarento.com>
2025-01-22 11:05:55 +0530

    notification route added

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use((req, _res, next) => {
 app.use(attachIo(io));
 app.use(cors());
 app.use(express.json());
-app.use('/notifications', notificationRoutes);
+app.use('/v1', notificationRoutes);
 setupSocket(io);
 
 httpServer.listen(applicationPort, () => {
